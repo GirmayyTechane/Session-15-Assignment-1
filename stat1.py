31da38799a0cc9822feb8fa46c73b220d2f032cb
GirmayyTechane <43317998+GirmayyTechane@users.noreply.github.com>
2018-11-25 14:09:32 +0100

    Update stat1.py

--- stat1.py
+++ stat1.py
@@ -1,1 +1,33 @@
 
+#Session 15: Assignment 1
+#Problem Statement 1:
+You survey households in your area to find the average rent they are paying. Find the
+standard deviation from the following data:
+$1550, $1700, $900, $850, $1000, $950.  
+#Answer :
+ first this is a sample standard deviation as it states that they will want to show how their sample results can be generalised to the whole population hence we will use the following steps 
+Step 1: Find the mean:
+($1550 + $1700 + $900 + $850 + $1000 + $950)/6 = $1158.33
+
+Step 2: Subtract the mean from each value. This gives you the differences:
+$1550 – $1158.33 = $391.67
+$1700 – $1158.33 = $541.67
+$900 – $1158.33 = -$258.33
+$850 – $1158.33 = -$308.33
+$1000 – $1158.33 = $158.33
+$950 – $1158.33 = $208.33
+
+Step 3: Square the differences you found in Step 3:
+$391.672 = 153405.3889
+$541.672 = 293406.3889
+-$258.332 = 66734.3889
+-$308.332 = 95067.3889
+$158.332 = 25068.3889
+$208.332 = 43401.3889
+
+Step 4: Add up all of the squares you found in Step 3 and divide by 5 (which is 6 – 1):
+(153405.3889 + 293406.3889 + 66734.3889 + 95067.3889 + 25068.3889 + 43401.3889) / 5 = 135416.66668
+
+Step 5: Find the square root of the number you found in Step 4 (the variance):
+√135416.66668 = 367.99
+The standard deviation is 367.99.
